1bf540028dadcd9993bdadf3f601d22f82f7fdfb
Yamngiem <Jasmin.Hietala@edu.savonia.fi>
2023-11-14 22:30:03 +0200

    melkein valmis talteen

---
+++ nook/src/App.js
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+import './login.css';
+
+
+
+function App() {
+
+  // const backgroundStyle = {
+  //   backgroundImage: `url(${process.env.PUBLIC_URL}/kuvat/login.jpg)`,
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  //   height: '100vh',
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   color: 'white',
+  // };
+
+
+
+
+
+
+  return (
+    <div className="grid-container">
+
+
+
+      <img src="../kuvat/logo.png" alt="Logo kuvaileva teksti" className="logo" />
+
+
+      <label class="username">
+
+        <input defaultValue={"text"} id="etunimi" type="text" placeholder="Username" />      </label>
+
+
+
+      <label class="password">
+
+        <input defaultValue={"password"} id="etunimi" type="text" placeholder="Enter your password" />      </label>
+
+
+      <button className="loginnappi"></button>
+      {/* Täällä voit lisätä grid-item-elementtejä */}
+    </div>
+  );
+}
+
+export default App;
