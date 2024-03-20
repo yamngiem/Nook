ee39c6de1d4b43af58ec8d0f7dc2675b47853cb8
Yamngiem <Jasmin.Hietala@edu.savonia.fi>
2024-03-20 22:12:59 +0200

    lisätty sivuun containerit

---
+++ nook/src/Display.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Display extends React.Component {
+  render() {
+    return <input type="text" className="display" />;
+  }
+}
+
+export default Display;
--- nook/src/work.js
+++ nook/src/work.js
@@ -4,7 +4,25 @@ import './work.css'; // Lisää import-lauseeseen "from"
 
 function Work() {
     return (
-        <div className='workelements'></div>
+        <div className='workelements'>
+            <a href="/page2"  className='button60' id='btn4' > X </a>
+            <div className='calculator'>
+                <h1>laskin</h1>
+            </div>
+            <div className='spotify'>
+                <h1>spotify</h1>
+            </div>
+            <div className='weather'>
+                <h1>sää</h1>
+            </div>
+            <div className='wildcard'>
+                <h1>willikortti</h1>
+            </div>
+            <div className='game'>
+                <h1>peli</h1>
+            </div>
+
+        </div>
 
         
     
